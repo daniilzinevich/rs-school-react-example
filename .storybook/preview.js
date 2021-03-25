488b74b08963e71ad94425a8997a266d1684f6ec
Daniil Zinevich <danila2412@gmail.com>
2021-03-25 15:37:50 +0300

    Add decorator

--- .storybook/preview.js
+++ .storybook/preview.js
@@ -1,3 +1,16 @@
+// .storybook/preview.js
+
+import React from 'react';
+
+export const decorators = [
+  (Story) => (
+    <div className="App">
+      <header className="App-header">
+        <Story />
+      </header>
+    </div>
+  ),
+];
 
 export const parameters = {
   actions: { argTypesRegex: "^on[A-Z].*" },
